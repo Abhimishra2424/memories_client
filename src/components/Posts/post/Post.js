cb8006090cf21582ca2df07ef8b52c4756194837
Abhishek Mishra <abmishra700@gmail.com>
2022-07-04 11:44:08 +0530

    likes logic if user is not created same post so then hide like and delete btn

--- src/components/Posts/post/Post.js
+++ src/components/Posts/post/Post.js
@@ -6,23 +6,49 @@ import {
   CardMedia,
   Button,
   Typography,
-  Tooltip,
 } from "@material-ui/core/";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
+import { useDispatch } from "react-redux";
 import moment from "moment";
 
+import { likePost, deletePost } from "../../../actions/posts";
 import useStyles from "./styles";
 
-import { useDispatch } from "react-redux";
-
-import { deletePost, likePost } from "../../../actions/posts";
-
 const Post = ({ post, setCurrentId }) => {
-  const classes = useStyles();
-
   const dispatch = useDispatch();
+  const classes = useStyles();
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  const Likes = () => {
+    if (post.likes.length > 0) {
+      return post.likes.find(
+        (like) => like === (user?.result?.googleId || user?.result?._id)
+      ) ? (
+        <>
+          <ThumbUpAltIcon fontSize="small" />
+          &nbsp;
+          {post.likes.length > 2
+            ? `You and ${post.likes.length - 1} others`
+            : `${post.likes.length} like${post.likes.length > 1 ? "s" : ""}`}
+        </>
+      ) : (
+        <>
+          <ThumbUpAltOutlined fontSize="small" />
+          &nbsp;{post.likes.length} {post.likes.length === 1 ? "Like" : "Likes"}
+        </>
+      );
+    }
+
+    return (
+      <>
+        <ThumbUpAltOutlined fontSize="small" />
+        &nbsp;Like
+      </>
+    );
+  };
 
   return (
     <Card className={classes.card}>
@@ -40,15 +66,17 @@ const Post = ({ post, setCurrentId }) => {
           {moment(post.createdAt).fromNow()}
         </Typography>
       </div>
-      <div className={classes.overlay2}>
-        <Button
-          style={{ color: "white" }}
-          size="small"
-          onClick={() => setCurrentId(post._id)}
-        >
-          <MoreHorizIcon fontSize="default" />
-        </Button>
-      </div>
+      {user?.result?._id === post?.creator && (
+        <div className={classes.overlay2}>
+          <Button
+            onClick={() => setCurrentId(post._id)}
+            style={{ color: "white" }}
+            size="small"
+          >
+            <MoreHorizIcon fontSize="default" />
+          </Button>
+        </div>
+      )}
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary" component="h2">
           {post.tags.map((tag) => `#${tag} `)}
@@ -68,24 +96,23 @@ const Post = ({ post, setCurrentId }) => {
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Tooltip title="Like Karo" arrow>
+        <Button
+          size="small"
+          color="primary"
+          disabled={!user?.result}
+          onClick={() => dispatch(likePost(post._id))}
+        >
+          <Likes />
+        </Button>
+        {user?.result?._id === post?.creator && (
           <Button
             size="small"
-            color="primary"
-            onClick={() => dispatch(likePost(post._id))}
-          >
-            <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}{" "}
-          </Button>
-        </Tooltip>
-        <Tooltip title="delete maro" arrow>
-          <Button
-            size="small"
-            color="primary"
+            color="secondary"
             onClick={() => dispatch(deletePost(post._id))}
           >
             <DeleteIcon fontSize="small" /> Delete
           </Button>
-        </Tooltip>
+        )}
       </CardActions>
     </Card>
   );
