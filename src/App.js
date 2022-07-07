59c93a430353597940d4432e4cc076e6ade2cc68
Abhishek Mishra <abmishra700@gmail.com>
2022-07-07 17:57:15 +0530

    aaded pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route  } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-import { getPosts } from '../../actions/posts';
-import Posts from '../Posts/Posts';
-import Form from '../Form/Form';
+import { getPosts } from "../../actions/posts";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+import Pagination from "../Pagination";
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
@@ -17,12 +18,20 @@ const Home = () => {
   return (
     <Grow in>
       <Container>
-        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
           <Grid item xs={12} sm={7}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
           <Grid item xs={12} sm={4}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper elevation={6}>
+              <Pagination />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
